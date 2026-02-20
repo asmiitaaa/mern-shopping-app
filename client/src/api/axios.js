cdba69533cbdfbe519d0d50cc47e99235ff690a7
asmiitaaa <asmita20six@gmail.com>
2026-02-20 11:04:03 +0530

    Prepared frontend for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-shopping-app-29ob.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
